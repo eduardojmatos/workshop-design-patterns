c23a104d76900208ae0d91b9ad9b24e8c76d685f
Eduardo Matos <eduardoj.matos@gmail.com>
2017-03-22 12:31:17 -0300

    Subject files to observer pattern

---
+++ src/observer/subject.js
@@ -0,0 +1,34 @@
+var Subject = function() {
+
+  return {
+    observers: [],
+
+    subscribe: function(observer) {
+      this.observers.push(observer);
+    },
+
+    unsubscribe: function(observer) {
+      const index = this.observers.indexOf(observer);
+
+      if (index > -1) {
+        this.observers.splice(index, 1);
+      }
+    },
+
+    notify: function(observer) {
+      const index = this.observers.indexOf(observer);
+
+      if (index > -1) {
+        this.observers[index].notify(index);
+      }
+    },
+
+    notifyAll: function() {
+      for (let i = 0; i < this.observers.length; i++){
+        this.observers[i].notify(i);
+      };
+    }
+  };
+};
+
+module.exports = Subject;
---
+++ test/observer/subject.js
@@ -0,0 +1,64 @@
+import expect from "expect.js";
+//const expect = require("expect.js");
+const sinon = require("sinon");
+const Subject = require("../../build/observer/subject.js");
+
+describe("Subject Module", function () {
+  let observer1 = new Object();
+  let observer2 = new Object();
+  let observer3 = new Object();
+
+  describe("#initialize", function () {
+
+    describe("#subscribe", function () {
+      const subject = new Subject();
+
+      it("add observer to subject", function () {
+        subject.subscribe(observer1);
+        expect(subject.observers.length).to.be(1);
+        expect(subject.observers[0]).to.be(observer1);
+      });
+    });
+
+    describe("#unsubscribe", function () {
+      const subject = new Subject();
+
+      it("removes observer from subject", function () {
+        subject.unsubscribe(observer1);
+        expect(subject.observers.length).to.be(0);
+      });
+    });
+
+    describe("#notify", function () {
+      const subject = new Subject();
+
+      observer1.notify = sinon.stub().returns("notified");
+      subject.subscribe(observer1);
+
+      it("calls notify of observer", function () {
+        subject.notify(observer1);
+        expect(observer1.notify).to.be.called;
+      });
+    });
+
+    describe("#notifyAll", function () {
+      const subject = new Subject();
+
+      observer1.notify = sinon.stub().returns("notified");
+      observer2.notify = sinon.stub().returns("notified");
+      observer3.notify = sinon.stub().returns("notified");
+
+      subject.subscribe(observer1);
+      subject.subscribe(observer2);
+      subject.subscribe(observer3);
+
+      it("calls all observers notify", function () {
+        //subject.notifyAll();
+        expect(observer1.notify).to.be.called;
+        expect(observer2.notify).to.be.called;
+        expect(observer3.notify).to.be.called;
+      });
+    });
+  });
+});
+
