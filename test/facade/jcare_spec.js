cf3cba122bd91921105824da5566b3713bcbce76
Eduardo Matos <eduardoj.matos@gmail.com>
2018-11-28 21:41:13 -0200

    .

--- test/facade/jcare_spec.js
+++ test/facade/jcare_spec.js
@@ -1,44 +1,44 @@
-import expect from "expect.js";
+import { expect, describe, it } from "expect.js";
 import sinon from "sinon";
 import jsdom from "mocha-jsdom";
-import jCare from "../../src/facade/jcare.js";
+import jCare from "../../src/facade/jcare";
 
-describe("jCare", function () {
+describe("jCare", function() {
   jsdom();
 
-  before(function () {
+  before(function() {
     this.foundedElement = document.createElement("div");
     this.foundedElement.classList.add("test");
     document.body.appendChild(this.foundedElement);
   });
 
-  describe("instantiate jCare", function () {
-    it("without document argument", function () {
+  describe("instantiate jCare", function() {
+    it("without document argument", () => {
       expect(jCare).to.throwException(/document needs to be passed as argument/);
     });
 
-    it("with document argument", function () {
+    it("with document argument", () => {
       expect(jCare).withArgs(document).to.not.throwException();
     });
   });
 
-  describe("#findClass", function () {
+  describe("#findClass", () => {
     let $;
 
     beforeEach(function () {
       $ = new jCare(document);
     });
 
-    it("with argument return elements", function () {
+    it("with argument return elements", () => {
       expect($.findClass("test")).not.to.be.empty;
       expect($.findClass("test")[0].classList).to.be(this.foundedElement.classList);
     });
 
-    it("without argument returns error", function () {
+    it("without argument returns error", () => {
       expect($.findClass).to.throwException(/a class needs to be passed/);
     });
 
-    it("uses querySelectorAll if browser supports", function () {
+    it("uses querySelectorAll if browser supports", () => {
       const querySelectorAll = sinon.spy(document, "querySelectorAll");
 
       $.findClass("test");
@@ -46,7 +46,7 @@ describe("jCare", function () {
       sinon.assert.calledOnce(querySelectorAll);
     });
 
-    it("uses getElementsByClassName if browser dont supports querySelectorAll", function () {
+    it("uses getElementsByClassName if browser dont supports querySelectorAll", () => {
       $ = new jCare(document);
       document.querySelectorAll = null;
 
